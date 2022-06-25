d1a6f081d9f054fcf847dab00c11b9ca48df2eec
JJunio <junio.araujo01@yahoo.com>
2022-06-25 20:23:04 -0300

    Declaração de variaveis e criação da função para atualizar status dos equipamentos

--- Codigo_arduino/sketch/sketch.ino
+++ Codigo_arduino/sketch/sketch.ino
@@ -6,11 +6,34 @@ bool statusConexao = false;
 char* host = "https://esp8266-82004-default-rtdb.firebaseio.com/";
 char* token = "o5r4y9BSVdio6bLBwXy6F4NnVIFP7tUWq6kV7zQ4";
 
+#define lampadaQuarto D3;
+#define lampadaQuarto2 D5;
+#define lampadaSala D4;
+#define lampadaBanheiro D6;
+#define lamadaVaranda D7;
+#define rele D8;
+#define sensorTemperatura A0;
+
 FirebaseData minhaBase;
 
 
-void setup() {
+void atualizarStatus(int comodo, int novaSituacao) {
   
+  if (novaSituacao == 0) {
+    digitalWrite(comodo,HIGH);
+  } else {
+    digitalWrite(comodo,LOW);
+  }
+}
+
+void ligaArcondicionado() {
+  if(analogRead(sensorTemperatura > 20)) {
+    digitalWrite(rele,HIGH);
+  }
+}
+
+void setup() {
+
   Serial.begin(115200);
   pinMode(4,OUTPUT);
   WiFi.mode(WIFI_STA);
@@ -28,7 +51,24 @@ void setup() {
 void loop() {
   
   Firebase.get(minhaBase, "led/status");
+  Firebase.get(minhaBase, "quartoUm/status");
+  Firebase.get(minhaBase, "quartaDois/status");
+  Firebase.get(minhaBase, "sala/status");
+  Firebase.get(minhaBase, "banheiro/status");
+  Firebase.get(minhaBase, "varanda/status");
   int led = minhaBase.intData();
+  int quartoUm = minhaBase.intData();
+  int quartoDois = minhaBase.intData();
+  int sala = minhaBase.intData();
+  int banheiro = minhaBase.intData();
+  int varanda = minhaBase.intData();
+
+  atualizarStatus(lampadaQuarto,quartoUm);
+  atualizarStatus(lampadaQuarto2,quartoDois);
+  atualizarStatus(lampadaSala,sala);
+  atualizarStatus(lampadaBanheiro,banhiro);
+  atualizarStatus(lamadaVaranda,varanda);
+
 
   switch(led){
 
